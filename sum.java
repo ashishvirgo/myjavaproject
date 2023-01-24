ee8da527489ecc511dcddd328c1172830755e396
ashish bajpai <ashish.bajpai@ksolves.com>
2023-01-24 11:23:17 +0530

    i add modified sum.java file

--- sum.java
+++ sum.java
@@ -2,7 +2,6 @@ public class sum {
     public static void main(String[] args) {
         int a=10;
         int b=20;
-        int c=a+b;
-        System.out.println("sum="+c);
+        System.out.println("sum="+(a+b));
     }
 }
